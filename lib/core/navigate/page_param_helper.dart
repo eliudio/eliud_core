9ef34d92b490fd302723aa1149c252c313365abf
Eliud <58745656+eliudio@users.noreply.github.com>
2021-05-30 18:43:34 +0100

    feed fixed

---
+++ lib/core/navigate/page_param_helper.dart
@@ -0,0 +1,35 @@
+import 'package:eliud_core/core/access/bloc/access_bloc.dart';
+import 'package:flutter/cupertino.dart';
+
+class PageContextInfo {
+  final String appId;
+  final String pageId;
+  Map<String, dynamic>? parameters;
+
+  PageContextInfo(this.appId, this.pageId, {this.parameters});
+}
+
+class PageParamHelper {
+  static PageContextInfo getPagaContextInfo(BuildContext context) {
+    var modalRoute = ModalRoute.of(context) as ModalRoute;
+    var app = AccessBloc.app(context);
+    if (app == null) {
+      throw Exception('No app selected');
+    }
+    var settings = modalRoute.settings;
+    var appId = app.documentID;
+    if (appId == null) {
+      throw Exception('No app selected');
+    }
+    var fullPageId = settings.name;
+    if (fullPageId == null) {
+      throw Exception('No page selected');
+    }
+    var pageId = settings.name!.substring(appId!.length + 1);
+    Map<String, dynamic>? parameters;
+    if (settings.arguments != null) {
+      parameters = settings.arguments as Map<String, dynamic>;
+    }
+    return PageContextInfo(appId, pageId, parameters: parameters);
+  }
+}
