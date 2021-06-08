59766ee47034bda2331a94efea6dcfd03e0da478
Eliud <58745656+eliudio@users.noreply.github.com>
2021-06-08 16:18:55 +0100

    Introducing quick photosnap + bugfix

--- lib/tools/storage/fb_storage_image.dart
+++ lib/tools/storage/fb_storage_image.dart
@@ -2,15 +2,53 @@
 import 'dart:typed_data';
 
 import 'package:eliud_core/core/widgets/progress_indicator.dart';
+import 'package:eliud_core/model/member_medium_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
-class FbStorageImage extends StatefulWidget {
+class MemberImageModelWidget extends StatefulWidget {
+  final MemberMediumModel memberMediumModel;
+  final double? width;
+  final double? height;
+  final bool showThumbnail;
+  final Widget? defaultWidget;
+
+  const MemberImageModelWidget({Key? key, required this.memberMediumModel, this.width, this.height, required this.showThumbnail, this.defaultWidget, }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MemberImageModelState();
+  }
+}
+
+class _MemberImageModelState extends State<MemberImageModelWidget> {
+  @override
+  Widget build(BuildContext context) {
+    var url;
+    if (widget.showThumbnail) {
+      url = widget.memberMediumModel!.urlThumbnail!;
+    } else {
+      url = widget.memberMediumModel!.url!;
+    }
+    if ((url == null) && (widget.defaultWidget != null)) {
+      return widget.defaultWidget!;
+    }
+    return Image.network(
+      url,
+      width: widget.width,
+      height: widget.height,
+    );
+  }
+}
+
+
+// This doesn't seem to work as it seems sometimes the wrong image is picked up. Weird
+class FbStorageImageDoesntWork extends StatefulWidget {
   final String ref;
   final double? width;
   final double? height;
 
-  const FbStorageImage({Key? key, required this.ref, this.width, this.height, }) : super(key: key);
+  const FbStorageImageDoesntWork({Key? key, required this.ref, this.width, this.height, }) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -18,7 +56,7 @@ class FbStorageImage extends StatefulWidget {
   }
 }
 
-class _FbStorageImageState extends State<FbStorageImage> {
+class _FbStorageImageState extends State<FbStorageImageDoesntWork> {
   Future<Uint8List?>? future;
 
   @override
