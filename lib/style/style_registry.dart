0ec90f66f434b89311da75bc3521d9bd6c683862
Eliud <58745656+eliudio@users.noreply.github.com>
2021-08-09 18:20:31 +0100

    todo

--- lib/style/style_registry.dart
+++ lib/style/style_registry.dart
@@ -1,4 +1,5 @@
 import 'package:eliud_core/core/access/bloc/access_bloc.dart';
+import 'package:eliud_core/default_style/instances/eliud_eliud_style.dart';
 import 'package:eliud_core/default_style/instances/eliud_minkey_style.dart';
 import 'package:eliud_core/model/app_model.dart';
 import 'package:eliud_core/style/style.dart';
@@ -37,7 +38,8 @@ class StyleRegistry {
   }
 
   Style defaultStyle() {
-    _defaultStyle ??= EliudMinkeyStyle.defaultEliudStyle();
+//    _defaultStyle ??= EliudMinkeyStyle.defaultEliudStyle();
+    _defaultStyle ??= EliudEliudStyle.defaultEliudStyle();
     return _defaultStyle!;
   }
 
