8016c17dbbed4f40ff84d300892c9667249ee5bf
Eliud <58745656+eliudio@users.noreply.github.com>
2021-08-15 10:30:36 +0100

    Creator button and creator package plan

--- lib/creator/widgets/creator_mode.dart
+++ lib/creator/widgets/creator_mode.dart
@@ -4,12 +4,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CreatorMode extends StatefulWidget {
+  static double BUTTON_HEIGHT = 35;
+  static Color BACKGROUND_COLOR = Colors.green.withOpacity(.3);
+  static Color ICON_COLOR = Colors.red;
+  static Color BORDER_COLOR = Colors.red;
+  static Color TEXT_COLOR = Colors.white;
+
   final bool edit;
-  final String label;
+  final String? label;
   final Widget toDecorate;
   final GlobalKey toDecorateKey;
 
-  CreatorMode({Key? key, required this.toDecorateKey, required this.edit, required this.toDecorate, required this.label})
+  CreatorMode({Key? key, required this.toDecorateKey, required this.edit, required this.toDecorate, this.label})
       : super(key: key);
 
   @override
@@ -17,16 +23,17 @@ class CreatorMode extends StatefulWidget {
     return CreatorModeState();
   }
 
-  static CreatorMode create({Key? key, required GlobalKey toDecorateKey, required bool edit, required Widget toDecorate, required String label}) {
+  static CreatorMode create({Key? key, required GlobalKey toDecorateKey, required bool edit, required Widget toDecorate, String? label}) {
     return CreatorMode(key: key, toDecorateKey: toDecorateKey, edit: edit, toDecorate: toDecorate, label: label);
   }
 }
 
 class CreatorModeState extends State<CreatorMode> {
   Offset? position = Offset(20, 20);
-  final double width = 100;
-  final double height = 20;
-  GlobalKey _buttonKey = GlobalKey();
+  double? width;
+  double? height;
+
+  final _buttonKey = GlobalKey();
 
   CreatorModeState();
 
@@ -47,8 +54,11 @@ class CreatorModeState extends State<CreatorMode> {
       var sizeBox = renderBox.size;
       var sizeButtonBox = renderButtonBox.size;
 
+      width = sizeButtonBox.width;
+      height = sizeButtonBox.height;
+
       setState(() {
-        position = Offset((sizeBox.width - sizeButtonBox.width) / 2, (sizeBox.height - sizeButtonBox.height) / 2);
+        position = Offset((sizeBox.width - width!) / 2, (sizeBox.height - height!) / 2);
       });
     }
   }
@@ -58,28 +68,49 @@ class CreatorModeState extends State<CreatorMode> {
     if (!widget.edit) {
       return widget.toDecorate;
     } else {
-      var button2 = Container(width: width, height: height, child: ElevatedButton.icon(
-          onPressed: () => print("aaaa"),
-          icon: Icon(
-            Icons.edit,
-            color: Colors.red,
-            size: 15,
-          ),
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.black.withOpacity(.1)),
-              padding: MaterialStateProperty.all(EdgeInsets.all(5)),
-              textStyle: MaterialStateProperty.all(TextStyle(fontSize: 30))),
-          label: Text(widget.label,
-              maxLines: 1,
-              style: TextStyle(
-                  fontSize: 10.0,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.white))));
+      var button;
+      var icon = Icon(
+        Icons.edit,
+        color: CreatorMode.ICON_COLOR,
+        size: 15,
+      );
+      if (widget.label == null) {
+        button = Container(height: CreatorMode.BUTTON_HEIGHT, width: CreatorMode.BUTTON_HEIGHT, decoration: BoxDecoration(
+            color: CreatorMode.BACKGROUND_COLOR,
+            border: Border.all(
+                width: 1,
+                color: CreatorMode.BORDER_COLOR
+            )),
+            child: GestureDetector(
+                onTap: () => _doIt(),
+                child: icon));
+      } else {
+        button = Container(height: CreatorMode.BUTTON_HEIGHT, decoration: BoxDecoration(
+            border: Border.all(
+                width: 1,
+                color: CreatorMode.BORDER_COLOR
+            )),
+            child: ElevatedButton.icon(
+                onPressed: () => _doIt(),
+                icon: icon,
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        CreatorMode.BACKGROUND_COLOR),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(5)),
+                    textStyle: MaterialStateProperty.all(
+                        TextStyle(fontSize: 30))),
+                label: Text(widget.label!,
+                    maxLines: 1,
+                    style: TextStyle(
+                        fontSize: 10.0,
+                        fontWeight: FontWeight.normal,
+                        color: CreatorMode.TEXT_COLOR))));
+      }
 
       var draggable = Draggable(
           key: _buttonKey,
-          feedback: button2,
-          child: button2,
+          feedback: button,
+          child: button,
           childWhenDragging: Container(),
           onDragEnd: (DraggableDetails details) {
             setState(() {
@@ -104,8 +135,12 @@ class CreatorModeState extends State<CreatorMode> {
       var newPosition =
       renderObject.globalToLocal(details.offset);
       position = Offset(
-          min(max(0, newPosition.dx), size.width - width), min(max(0, newPosition.dy), size.height - height));
+          min(max(0, newPosition.dx), size.width - width!), min(max(0, newPosition.dy), size.height - height!));
     }
+  }
+
+  void _doIt() {
+    print("aaaa");
   }
 }
 
