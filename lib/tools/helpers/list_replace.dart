2042a6960e9168570a1ce34a0ee9a60515c96bde
Eliud <58745656+eliudio@users.noreply.github.com>
2021-09-14 15:43:52 +0100

    Improved update menu

---
+++ lib/tools/helpers/list_replace.dart
@@ -0,0 +1,9 @@
+
+extension ListReplace<T> on List<T> {
+  void replace(int index1, T newItem) {
+    var length = this.length;
+    RangeError.checkValidIndex(index1, this, "index1", length);
+    this[index1] = newItem;
+  }
+}
+
